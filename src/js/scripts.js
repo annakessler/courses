4a6b181a778fed6bcbea20970fc9da84a0f22652
annakessler <kessler.anna96@gmail.com>
2021-12-07 17:26:42 +0300

    Доработка первого экрана

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,4 @@
 @include('libs/lazysizes.min.js');
 @include('libs/focus-visible.min.js');
 @include('functions/header.js');
-@include('libs/particles.min.js');
-@include('libs/app.js');
 @include('functions/spollers.js');
