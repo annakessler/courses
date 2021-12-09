a2ea2d2972ce24384e58e4e75e0cd9c0e7355ee4
annakessler <kessler.anna96@gmail.com>
2021-12-09 03:03:06 +0300

    Фикс формы

--- src/resources/telegram.php
+++ src/resources/telegram.php
@@ -1,13 +1,13 @@
 <?php
 
 $name = $_POST['name'];
-$email = $_POST['email'];
+$username = $_POST['username'];
 $msg = $_POST['msg'];
 $token = "5096731567:AAGi2jvRZm5GOHsOQkuc1LlKAdkxIEWEKPE";
 $chat_id = "-736192937";
 $arr = array(
-  'Имя пользователя: ' => $name,
-  'Email' => $email,
+  'Имя: ' => $name,
+  'Ник:' => $username,
   'Сообщение:' => $msg
 );
 
