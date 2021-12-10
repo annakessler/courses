a2fdbb75a4c8f034f69b846c2ca1559ed55b72b2
ZayRexan <88442043+ZayRexan@users.noreply.github.com>
2021-12-11 00:48:26 +0700

    Подключил from.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,6 @@
 @include('libs/wow.min.js');
 @include('functions/wow-init.js');
 @include('functions/header.js');
-@include('functions/mask.js');
+@include('functions/form.js');
 @include('functions/spollers.js');
 @include('functions/tabs.js');
