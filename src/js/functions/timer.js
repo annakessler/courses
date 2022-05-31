e625edba5e1213155d1b13fe4262825a8faba22b
annakessler <kessler.anna96@gmail.com>
2022-06-01 02:58:06 +0300

    Изменение таймера

--- src/js/functions/timer.js
+++ src/js/functions/timer.js
@@ -5,28 +5,28 @@ function getTimeRemaining(endtime) {
 	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 	var days = Math.floor(t / (1000 * 60 * 60 * 24));
 	return {
-		'total': t,
-		'days': days,
-		'hours': hours,
-		'minutes': minutes,
-		'seconds': seconds
+		total: t,
+		days: days,
+		hours: hours,
+		minutes: minutes,
+		seconds: seconds,
 	};
 }
 
 function initializeClock(id, endtime) {
 	var clock = document.getElementById(id);
-	var daysSpan = clock.querySelector('.days');
-	var hoursSpan = clock.querySelector('.hours');
-	var minutesSpan = clock.querySelector('.minutes');
-	var secondsSpan = clock.querySelector('.seconds');
+	var daysSpan = clock.querySelector(".days");
+	var hoursSpan = clock.querySelector(".hours");
+	var minutesSpan = clock.querySelector(".minutes");
+	var secondsSpan = clock.querySelector(".seconds");
 
 	function updateClock() {
 		var t = getTimeRemaining(endtime);
 
 		daysSpan.innerHTML = t.days;
-		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+		minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+		secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
 
 		if (t.total <= 0) {
 			clearInterval(timeinterval);
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
 	var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = '06/05/2022';
-initializeClock('countdown', deadline);
+var deadline = "06/15/2022";
+initializeClock("countdown", deadline);
